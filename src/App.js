8f12fadbc7bd35457b63d2e73c5787ef08de1e63
shashank pandey <pandey2001shashank@gmail.com>
2023-06-04 20:46:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Page0 from "./Components/Page0";
+import Card1 from "./Components/Card1";
+import Footer from './Components/Footer'
+import FAQs from './Components/FAQ'
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Page0 />
+      <Card1 />
+      <FAQs/>
+
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
